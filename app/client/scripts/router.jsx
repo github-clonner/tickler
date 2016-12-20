b09ada632d12e2fc53624d0a9824cb10906179cd
Benjamin Maggi <bmaggi@elementum.com>
2016-12-20 18:47:50 -0300

    changes

--- app/client/scripts/router.jsx
+++ app/client/scripts/router.jsx
@@ -2,7 +2,14 @@ import 'babel-polyfill';
 
 import React from 'react';
 import { render } from 'react-dom';
-import { Router, Route, IndexRoute } from 'react-router';
+import { Router, Route, hashHistory, browserHistory, IndexRoute } from 'react-router';
+import injectTapEventPlugin from 'react-tap-event-plugin';
+
+
+import App from './index';
+import HomePage from './components/home';
+import About from './components/about';
+import NotFound from './components/NotFound';
 
 /*const findDOMNode = ReactDOM.findDOMNode;
 const gui = require('nw.gui');
@@ -17,14 +24,20 @@ import EventEmitter from 'events';
 //import os from 'os';
 //import fs from 'fs';
 
-class Product {
-  constructor(connect) {
-    this.connect = connect;
-  }
+// Needed for onTouchTap
+// Can go away when react 1.0 release
+// Check this repo:
+// https://github.com/zilverline/react-tap-event-plugin
+injectTapEventPlugin();
 
-  request() {
-    return Promise.resolve(new Date())
-  }
-}
+render((
+  <Router history={browserHistory}>
+    <Route path="/" component={App} name="Container">
+    	<IndexRoute component={HomePage} name='Home'/>
+    </Route>
+    <Route path="/about" component={About}/>
+    <Route path="*" component={NotFound} />
+  </Router>
+), document.getElementById('app'))
 
 console.log('sdfgsdfg453345 worldxXXXX')
--- app/webpack.config.babel.js
+++ app/webpack.config.babel.js
@@ -1,14 +1,18 @@
 import path from 'path';
 import fs from 'fs';
 import webpack from 'webpack';
+import HtmlwebpackPlugin from 'html-webpack-plugin';
+import ExtractTextPlugin from 'extract-text-webpack-plugin';
 
 const babelSettings = JSON.parse(fs.readFileSync(".babelrc"));
+const config = JSON.parse(fs.readFileSync("package.json"));
+
+console.log(config);
 
 const env = {
   NODE_ENV: process.env.NODE_ENV || 'development',
 };
 
-console.log('PATH:', path.join(__dirname))
 export default {
   debug: true,
   entry: [
@@ -36,7 +40,14 @@ export default {
     new webpack.optimize.DedupePlugin(),
     new webpack.DefinePlugin({
       'process.env': JSON.stringify(env)
-    })
+    }),
+    new HtmlwebpackPlugin({
+      template: 'node_modules/html-webpack-template/index.ejs',
+      title: 'App',
+      appMountId: 'app',
+      inject: false
+    }),
+    //new ExtractTextPlugin('style.css', { allChunks: true })
   ],
   resolve: {
     extensions: [ '', '.js', '.jsx', '.coffee', '.less', '.ttf', '.eot', '.woff' ],
@@ -54,6 +65,16 @@ export default {
         loader: "file?name=[name].[ext]",
       },
       {
+        test: /\.css$/,
+        loaders: [
+          'style-loader',
+          'css-loader?importLoaders=1',
+          //'css-loader?modules&importLoaders=1&localIdentName=[name]__[local]___[hash:base64:5]!postcss-loader',
+          //'css-loader?modules&importLoaders=1&localIdentName=[name]__[local]___[hash:base64:5]',
+          'postcss-loader'
+        ]
+      },
+      {
         test: /\.jsx?$/,
         exclude: /node_modules/,
         loaders: [ 'react-hot', 'babel-loader' ],
@@ -63,6 +84,26 @@ export default {
           'babel?presets[]=latest&presets[]=react&presets[]=stage-0',
         ]*/
       }
+    ]
+  },
+  postcss: function (webpack) {
+    return [
+      require("postcss-import")({ addDependencyTo: webpack }),
+      require("postcss-url")(),
+      require("postcss-cssnext")({
+        browsers: [
+          'ie >= 10',
+          'Safari >= 7',
+          'ff >= 28',
+          'Chrome >= 34'
+        ]
+      }),
+      // add your "plugins" here
+      // ...
+      // and if you want to compress,
+      // just use css-loader option that already use cssnano under the hood
+      //require("postcss-browser-reporter")(),
+      //require("postcss-reporter")(),
     ]
   }
 }
