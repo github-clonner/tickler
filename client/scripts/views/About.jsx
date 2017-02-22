e25ae750b7d4020c836fa3e6c2fe2cc9fa947ef6
Benjamin Maggi <benjaminmaggi@gmail.com>
2017-02-22 07:47:15 -0300

    with electron-builder

--- client/scripts/views/About.jsx
+++ client/scripts/views/About.jsx
@@ -1,17 +1,11 @@
 // modules/About.js
 import React from 'react'
-import Editor from '../components/Editor';
 
 export default class About extends React.Component {
   render() {
     return (
       <div>
-        <Editor options={{
-          mode: 'javascript',
-          lineNumbers: true,
-          styleActiveLine: true,
-          theme: 'material'
-        }}/>
+        <h1>About</h1>
       </div>
     )
   }
